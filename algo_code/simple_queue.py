691e21e2eb2d43a6f099069f39fe8f56b7f9dbf3
Phurich <pusapanich.phurich@gmail.com>
2021-01-05 16:57:41 +1100

    Adding front viewer for queue urls

--- algo_code/simple_queue.py
+++ algo_code/simple_queue.py
@@ -7,12 +7,29 @@ Also defines UnitTests for the class.
 __author__ = "Maria Garcia de la Banda, modified by Brendon Taylor"
 __docformat__ = 'reStructuredText'
 
-import unittest
 from abc import ABC, abstractmethod
 from enum import Enum
 from typing import Generic
-from node import Node
-from referential_array import ArrayR, T
+# from node import Node
+from .referential_array import ArrayR, T
+
+
+class QueueSpitter:
+
+    has_queue = None
+    active_queue = None
+
+    def __init__(self):
+        """ Init nothing """
+
+    @classmethod
+    def make_queue(cls):
+        cls.active_queue = LinearQueue(max_capacity=10)
+        cls.active_queue.append(1)
+        cls.active_queue.append(2)
+        cls.active_queue.append(3)
+        cls.active_queue.append(4)
+        cls.has_queue = True
 
 
 class Queue(ABC, Generic[T]):
@@ -201,72 +218,72 @@ class CircularQueue(Queue[T]):
         return output
 
 
-class LinkQueue(Queue[T]):
-    """ Linked implementation of a queue with nodes.
-
-    Attributes:
-         length (int): number of elements in the linked queue (inherited)
-         front (int): reference to the front node (None represents an empty queue)
-         rear (int): reference to the rear node (None represents an empty queue)
-    """
-
-    def __init__(self, _=None) -> None:
-        Queue.__init__(self)
-        self.front = None
-        self.rear = None
-
-    def clear(self) -> None:
-        """ Resets the queue
-        :complexity: O(1)
-        """
-        super().clear()
-        self.front = None
-        self.rear = None
-
-    def is_full(self) -> bool:
-        """ Returns true if the list is full
-        :complexity: O(1)
-        """
-        return False
-
-    def append(self, item: T) -> None:
-        """ Adds an element to the rear of the queue.
-        :complexity: O(1)
-        """
-        new_node = Node(item)
-        if self.is_empty():
-            self.front = new_node
-        else:
-            self.rear.next = new_node
-        self.rear = new_node
-        self.length += 1
-
-    def serve(self) -> T:
-        """ Deletes and returns the element at the queue's front.
-        :pre: queue is not empty
-        :complexity: O(1)
-        :raises Exception: if the queue is empty
-        """
-        if self.is_empty():
-            raise Exception("Queue is empty")
-
-        temp = self.front.item
-        self.front = self.front.next
-        self.length -= 1
-        if self.is_empty():
-            self.rear = None
-        return temp
-
-    def __toString__(self):
-        """ Computes a string from the queue items - front to rear.
-        :complexity: O(N*M)
-            where N is the length of the list
-                  M is the size of the biggest item
-        """
-        result = ""
-        node = self.front
-        while node is not None:
-            result += str(node.item) + ", "
-            node = node.next
-
-        return result
+# class LinkQueue(Queue[T]):
+#     """ Linked implementation of a queue with nodes.
+#
+#     Attributes:
+#          length (int): number of elements in the linked queue (inherited)
+#          front (int): reference to the front node (None represents an empty queue)
+#          rear (int): reference to the rear node (None represents an empty queue)
+#     """
+#
+#     def __init__(self, _=None) -> None:
+#         Queue.__init__(self)
+#         self.front = None
+#         self.rear = None
+#
+#     def clear(self) -> None:
+#         """ Resets the queue
+#         :complexity: O(1)
+#         """
+#         super().clear()
+#         self.front = None
+#         self.rear = None
+#
+#     def is_full(self) -> bool:
+#         """ Returns true if the list is full
+#         :complexity: O(1)
+#         """
+#         return False
+#
+#     def append(self, item: T) -> None:
+#         """ Adds an element to the rear of the queue.
+#         :complexity: O(1)
+#         """
+#         new_node = Node(item)
+#         if self.is_empty():
+#             self.front = new_node
+#         else:
+#             self.rear.next = new_node
+#         self.rear = new_node
+#         self.length += 1
+#
+#     def serve(self) -> T:
+#         """ Deletes and returns the element at the queue's front.
+#         :pre: queue is not empty
+#         :complexity: O(1)
+#         :raises Exception: if the queue is empty
+#         """
+#         if self.is_empty():
+#             raise Exception("Queue is empty")
+#
+#         temp = self.front.item
+#         self.front = self.front.next
+#         self.length -= 1
+#         if self.is_empty():
+#             self.rear = None
+#         return temp
+#
+#     def __toString__(self):
+#         """ Computes a string from the queue items - front to rear.
+#         :complexity: O(N*M)
+#             where N is the length of the list
+#                   M is the size of the biggest item
+#         """
+#         result = ""
+#         node = self.front
+#         while node is not None:
+#             result += str(node.item) + ", "
+#             node = node.next
+#
+#         return result
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from algo_code.simple_stack import StackSpitter
+from algo_code.simple_queue import QueueSpitter
 app = Flask(__name__)
 
 
@@ -41,6 +42,25 @@ def simple_stack_pop():
 def simple_stack_length():
 
     return str(len(StackSpitter.active_stack))
+
+
+@app.route('/simple_queue')
+def simple_queue():
+
+    front = QueueSpitter.active_queue.front
+
+    return str(front) + ', Current front number is ' + str(QueueSpitter.active_queue.array[front])
+
+
+@app.route('/simple_queue/create')
+def simple_queue_create():
+
+    queue = QueueSpitter.make_queue()
+
+    return redirect(url_for('simple_queue'))
+
+
+
 
 
 
